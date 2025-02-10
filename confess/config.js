46a11424c27258dff96a465e64985804603a6d13
Lynch <161128706+LYNCH0617@users.noreply.github.com>
2025-02-10 22:45:56 +0800

    Add files via upload

---
+++ confess/config.js
@@ -0,0 +1,21 @@
+const CONFIG = {
+    titleWeb: "Valentines",
+    introTitle: 'Hii Love',
+    introDesc: `The moment our eyes met, the rest of the world blurred into the background. 
+    It wasn’t just a fleeting moment—it was a collision of past, present, and future, all converging in the space between us. 
+    My heart didn’t ask for permission, didn’t wait for logic to catch up. 
+    It simply knew, in a way that words could never explain.
+     And in that knowing, I found something I never knew I was missing.`,
+    btnIntro: '^^open^^',
+    title: 'Was it love at first sight? 🥰',
+    desc: 'Angels wove a thread of gold, A love ordained, a tale retold. Not bound by time, nor lost in space, But guided by a hand of grace.',
+    btnYes: 'yes <33',
+    btnNo: 'no :3',
+    question: 'There are more than 8 billion people in the world, why do you love me? <3',
+    btnReply: 'Send <3',
+    reply: 'Kasi ikaw na yan ehhh, alangan lilingon pa ako sa iba. <33333333',
+    mess: 'I know 🥰. Love you so much 😘😘',
+    messDesc: 'Cafe tommorow during your vacant.',
+    btnAccept: 'Okiiiii love <3',
+    
+}
